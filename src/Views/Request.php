f0c2f840e7892bcb3baebe5f185a94f274f1cd33
maliniravindranath2024 <malini.ravindranath@eleve.isep.fr>
2024-12-04 14:29:53 +0100

    First commit

--- src/Views/Request.php
+++ src/Views/Request.php
@@ -1,1 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pet Care Connect - Find trusted sitters</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/PAWCARE/public/css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <meta name="author" content="Group G2D">
+    <meta name="description" content="Connect with reliable pet sitters and caretakers.">
+
+</head>
+
+
+<body>
+<!-- Navigation -->
+<?php require_once (dirname(__DIR__).'\Views\components\navbar.php'); ?>
+
+<!-- Main Content -->
+<main>
+    <section class="hero">
+
+
+
+
+    </section>
+</main>
+
+<script src="/PAWCARE/public/js/main.js"></script>
+
+<?php require_once (dirname(__DIR__).'\Views\components\footer.php'); ?>
+
+</body>
+</html>
