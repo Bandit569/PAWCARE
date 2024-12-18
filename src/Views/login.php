7d567ae8b537049f85f33804802622238da0c448
maliniravindranath2024 <malini.ravindranath@eleve.isep.fr>
2024-12-18 23:25:58 +0100

    Further commit for Benjamin for troubleshooting

--- src/Views/login.php
+++ src/Views/login.php
@@ -5,12 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/PAWCARE/public/css/stylesheet.css">
-    <link rel="stylesheet" href="/PAWCARE/public/css/loginStyle.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <meta name="author" content="Group G2D">
     <meta name="description" content="Connect with reliable pet sitters and caretakers.">
 
 </head>
+
 
 <body>
 <!-- Navigation -->
@@ -23,25 +23,35 @@
             <img src="/PAWCARE/public/Images/GPTMyLove.webp" alt="Cat">
         </div>
         <div class="hero-text">
-            <h1>Welcome to Pawcare!</h1>
+            <h1>Login to PawCare</h1>
+            <form action="/Login" method="POST">
+                <label for="username">Username</label>
+                <input type="username" name="username" id="username" required>
 
-            <form class="form-container" action="/PAWCARE/login" method="POST">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required>
 
-                        <label class="form-group" for="userid">User ID</label>
-                        <input class="form-group" type="text" name="userid" required>
-                <label class="form-group" for="password">Password</label>
-                <input class="form-group" type="password" name="password" required>
-
-                <button class="Reqbutton" type="submit">Login</button>
-
+                <button type="submit">Login</button>
             </form>
 
+            <p>Don't have an account? <a href="/Register" class="cta-button">Register here</a></p>
+
+
+
+        <!--    <p>Your trusted partner in finding pet caretakers.</p>
+            <a href="/services" class="cta-button">Explore Services</a>-->
         </div>
-        <div class="lemon-cake-image">
+<!--        <div class="lemon-cake-image">
+
             <img src="/PAWCARE/public/Images/LemonCakesMyLove.webp" alt="Lemon Cake">
-        </div>
+        </div>-->
     </section>
+
+<!--    <section class="I need a herooo">
+
+    </section>-->
 </main>
+
 
 <?php require_once (dirname(__DIR__).'\Views\components\footer.php'); ?>
 
